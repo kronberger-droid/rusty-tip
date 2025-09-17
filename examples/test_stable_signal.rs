62dfa85be9be48882313e0a44305e0b071e42e0f
kronberger-droid <kronberger@proton.me>
2025-09-17 10:51:18 +0200

    prepare for testing

--- examples/test_stable_signal.rs
+++ examples/test_stable_signal.rs
@@ -1,57 +1,55 @@
-use rusty_tip::{
-    stability,
-    types::{DataToGet, TriggerConfig},
-    ActionDriver, NanonisClient,
-};
+use rusty_tip::{stability, ActionDriver};
 use std::{error::Error, time::Duration};
 use textplots::{Chart, Plot};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut driver = ActionDriver::new("127.0.0.1", 6501)?;
 
-    let signal = rusty_tip::SignalIndex(0);
+    let _freq_shift = rusty_tip::SignalIndex(76);
 
-    // driver.execute(rusty_tip::Action::Withdraw {
-    //     wait_until_finished: true,
-    //     timeout: Duration::from_secs(5),
-    // })?;
+    let z_pos = rusty_tip::SignalIndex(30);
 
-    let mut client = NanonisClient::new("127.0.0.1", 6502)?;
+    driver.execute(rusty_tip::Action::Withdraw {
+        wait_until_finished: true,
+        timeout: Duration::from_secs(5),
+    })?;
 
-    client.osci1t_trig_set(1, 0, 49.0e-12, 0.0)?;
+    driver.execute(rusty_tip::Action::AutoApproach {
+        wait_until_finished: true,
+    })?;
 
-    let (_, _, _, data) = client.osci1t_data_get(0)?;
+    if let Some(osci_data) = driver.read_oscilloscope_with_stability(
+        z_pos,
+        // Some(TriggerConfig::new(
+        //     rusty_tip::types::OsciTriggerMode::Level,
+        //     rusty_tip::TriggerSlope::Falling,
+        //     49.0e-12,
+        //     0.0,
+        // )),
+        None,
+        rusty_tip::types::DataToGet::Stable {
+            readings: 1,
+            timeout: Duration::from_secs(2),
+        },
+        stability::trend_analysis_stability,
+    )? {
+        let frame: Vec<(f32, f32)> = osci_data
+            .data
+            .iter()
+            .enumerate()
+            .map(|(i, &value)| (i as f32 * osci_data.dt as f32, value as f32))
+            .collect();
 
-    println!("{data:?}");
+        let max_time = (osci_data.size - 1) as f64 * osci_data.dt;
 
-    // if let Some(osci_data) = driver.read_oscilloscope(
-    //     signal,
-    //     // Some(TriggerConfig::new(
-    //     //     rusty_tip::types::OsciTriggerMode::Level,
-    //     //     rusty_tip::TriggerSlope::Falling,
-    //     //     49.0e-12,
-    //     //     0.0,
-    //     // )),
-    //     None,
-    //     rusty_tip::types::DataToGet::Current,
-    // )? {
-    //     let frame: Vec<(f32, f32)> = osci_data
-    //         .data
-    //         .iter()
-    //         .enumerate()
-    //         .map(|(i, &value)| (i as f32 * osci_data.dt as f32, value as f32))
-    //         .collect();
+        Chart::new(140, 60, 0.0, max_time as f32)
+            .lineplot(&textplots::Shape::Lines(&frame))
+            .nice();
 
-    //     let max_time = (osci_data.size - 1) as f64 * osci_data.dt;
+        let is_stable = stability::trend_analysis_stability(&osci_data.data);
 
-    //     Chart::new(140, 60, 0.0, max_time as f32)
-    //         .lineplot(&textplots::Shape::Lines(&frame))
-    //         .nice();
-
-    //     let is_stable = stability::trend_analysis_stability(&osci_data.data);
-
-    //     println!("{is_stable}");
-    // };
+        println!("{is_stable}");
+    };
 
     Ok(())
 }
